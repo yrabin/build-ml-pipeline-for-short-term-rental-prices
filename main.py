7f17585ce7e187b9b8332dbfdc9e5446440b0318
yrabin <yusak.rabin@gmail.com>
2023-06-19 12:09:57 +0000

    commit after all steps

--- main.py
+++ main.py
@@ -82,12 +82,14 @@ def go(config: DictConfig):
             )
 
         if "data_split" in active_steps:
+            print("data split")
             ##################
             # Implement here #
             ##################
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
+                version='main',
                 parameters={
                     "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
@@ -109,16 +111,35 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
-            pass
-
+            _ = mlflow.run(
+                os.path.join(
+                    hydra.utils.get_original_cwd(),
+                    "src",
+                    "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"},
+            )
+            
         if "test_regression_model" in active_steps:
 
             ##################
             # Implement here #
             ##################
-
-            pass
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                version='main',
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"},
+            )
 
 if __name__ == "__main__":
     go()
